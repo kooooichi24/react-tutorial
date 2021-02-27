3100339bff18ae49b172c8117119eded109c59cd
古川広一 <ex.kouichi1224@gmail.com>
2021-02-27 15:46:11 +0900

    Fix: snapshotsとimageSnapshotsの設定ファイルを分離した

--- src/Storybook.test.js
+++ src/Storybook.test.js
@@ -1,6 +1,5 @@
 // storybook.test.js
 
 import initStoryshots from '@storybook/addon-storyshots';
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() });
+initStoryshots();
